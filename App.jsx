85e8f4b54bece1ee1e62e7b3e395c22bdcae8df9
Azraa <24000.IT@rmkec.ac.in>
2025-07-29 14:36:17 +0530

    Hooks

---
+++ App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Header from "./pages/Header/Header";
+import HeroSection from "./pages/HeroSection/HeroSection";
+import Card from "./pages/Card/Card";
+import Footer from "./pages/Footer/Footer";
+import"flowbite";
+import ProductsArray from "./pages/Card/ProductsArray";
+
+const App= () => {
+  return (
+    <>
+   <Header />
+    <HeroSection />  
+    {
+      Card.map((item) =>{
+        return(
+          
+          <Card  
+          key={item.id}
+          title={item.title}
+          price={item.price}
+          offer={item.offer}/>
+          
+        );
+      })
+    }
+ <Footer/>
+    </>
+  );
+};
+export default App;
---
+++ Counter/Counter.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+
+const Counter = () => {
+    let[val,setVal]=useState(10);
+    let[name,setName]=useState("sam");
+     let[valid,setValid]=useState(true); 
+
+  return (
+    <>
+    <div className="2xl:container mx-auto  border border-red-800 " >
+        <div className="w-[70%] mx-auto ">
+    
+        <div className=" flex flex-col gap-7">
+            <h1 className="font-bold text-[30px]">Counter</h1>
+            <p className="font-bold text-[30px]">{val}</p>
+            <div className="flex gap-8">
+                <button className="bg-red-600 px-9 py-3 rounded-lg "
+                onClick={ () =>{
+                    setVal(val-1);
+                    console.log(val);
+                    }
+                    }
+                    >-</button>
+                <button className="bg-green-600 px-9 py-3 rounded-lg" 
+                 onClick={ () =>{
+                    setVal(val+1);
+                    console.log(val);
+                    }
+                    }
+                    >+</button>
+            </div>
+            
+        </div>
+    
+    </div>
+    </div>
+
+
+
+
+    <div className="border border-red-800 space-y-7 ">
+        <p>{name}</p>
+        <button className="bg-green-600 px-9 py-3 rounded-lg "
+                onClick={ () =>{
+                    setName("Azraa");
+                    
+                    }
+                    }>Change</button>
+
+    </div>
+
+
+
+    <div className="border border-red-800 space-y-7 ">
+        <p>{valid? "✅" :"❌" }</p>
+        <button className="bg-green-600 px-9 py-3 rounded-lg "
+                onClick={ () =>{
+                    setValid(!  valid);
+                    
+                    }
+                    }>Change</button>
+
+    </div>
+    </>
+    
+  )
+}
+
+export default Counter
